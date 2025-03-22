79761955c4e2268d58c4aba62488d2d2060e5546
Shivansh Joshi <shivanshjoshi2922@gamil.com>
2025-03-22 18:52:10 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@ dotenv.config({
     path: './env'
 })
 
+
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at prt : ${process.env.PORT}`);
+
+        })
+    })
+    .catch((error) => {
+        console.log("MONGO DB Connection failed !!! ", err);
+    })
 
 
 

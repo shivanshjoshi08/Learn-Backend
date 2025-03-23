d5dd46f73c0aeae9f6b25bc7bc32c7116fd569bb
Shivansh Joshi <shivanshjoshi2922@gamil.com>
2025-03-23 12:13:55 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 // require('dotenv').config()
 import dotenv from "dotenv"
-
+// import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-
+// const app = express();
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running at prt : ${process.env.PORT}`);
+            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 
         })
     })
     .catch((error) => {
-        console.log("MONGO DB Connection failed !!! ", err);
+        console.log("MONGO DB Connection failed !!! ", error);
     })
 
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs'
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uplaodOnCloudinary = async(localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null
+        }
+         // Upload file to Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("file is uploaded on cloudinary,response.url");
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation gotfailed
+        return null;
+    }
+}
+
+
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia.commons/a/ae/Olympic_flag.jpg", { public_id: "olympic_flag" }, function (error, result) {
+//     console.log(result);
+// });
+
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia.commons/a/ae/Olympic_flag.jpg", { public_id: "olympic_flag" }, function (error, result) {
+//     console.log(result);
+// });
